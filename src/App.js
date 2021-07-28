583db2d9edfb52409e91071f2de9c66b70d2d0ce
Kyle Lynch <kylelynch104@gmail.com>
2021-07-28 17:17:06 -0500

    apollo/client graphql, single review page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
 import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Header from './components/Header'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
+//  page and layout imports
+import Header from './components/Header'
 const Homepage = lazy(() => import('./pages/Homepage'))
 const ReviewDetails = lazy(() => import('./pages/ReviewDetails'))
 const Category = lazy(() => import('./pages/Category'))
 
+//  apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache(),
+})
 
 const App = () => {
   return (
     <Router>
-      <div className='app'>
-        <Header />
-        <Switch>
-          <Route exact path='/' render={() => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <Homepage />
-            </Suspense>
-          )} />
-          <Route exact path='/details/:id' render={() => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <ReviewDetails />
-            </Suspense>
-          )} />
-          <Route exact path='/category/:id' render={() => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <Category />
-            </Suspense>
-          )} />
-        </Switch>
-      </div>
+      <ApolloProvider client={client}>
+        <div className='app'>
+          <Header />
+          <Switch>
+            <Route
+              exact
+              path='/'
+              render={() => (
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Homepage />
+                </Suspense>
+              )}
+            />
+            <Route
+              exact
+              path='/details/:id'
+              render={() => (
+                <Suspense fallback={<div>Loading...</div>}>
+                  <ReviewDetails />
+                </Suspense>
+              )}
+            />
+            <Route
+              exact
+              path='/category/:id'
+              render={() => (
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Category />
+                </Suspense>
+              )}
+            />
+          </Switch>
+        </div>
+      </ApolloProvider>
     </Router>
-
   )
 }
 
--- src/pages/ReviewDetails.jsx
+++ src/pages/ReviewDetails.jsx
@@ -1,8 +1,22 @@
+import { useParams, Link } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
 
 const ReviewDetails = () => {
+    const { id } = useParams()
+    const { data, error, loading } = useFetch(`http://localhost:1337/reviews/${id}`)
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error :(</p>
+
     return (
-        <div>
-            Review Details
+        <div className='review-card'>
+            <div className="rating">{data.rating}</div>
+            <h2>{data.title}</h2>
+            <small>console list</small>
+
+            <p>{data.body}</p>
+
+            <Link to='/'>Back to Homepage</Link>
         </div>
     )
 }
